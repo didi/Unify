ff3757f46391e5e093739ffad16981d2473a5e47
Maxiee <maxieewong@gmail.com>
2024-07-04 16:57:18 +0800

    补充 Android 统一调用示例

--- example/01_uninativemodule_demo/example/android/app/src/main/java/com/example/uninativemodule_demo_example/MainActivity.java
+++ example/01_uninativemodule_demo/example/android/app/src/main/java/com/example/uninativemodule_demo_example/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.uninativemodule_demo_example;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 
+import com.example.uninativemodule_demo.DeviceInfoService;
 import com.example.uninativemodule_demo.DeviceInfoServiceRegister;
+import com.example.uninativemodule_demo.UDUniAPI;
 
 public class MainActivity extends FlutterActivity {
 
@@ -17,5 +21,9 @@ public class MainActivity extends FlutterActivity {
         DeviceInfoServiceRegister.setup(
                 flutterEngine.getDartExecutor(),
                 new DeviceInfoServiceImpl());
+
+        // 在 Android 端以统一调用方式调用 Unify 模块
+        UDUniAPI.get(DeviceInfoService.class).getDeviceInfo(
+                result -> Log.d("Unify", result.toMap().toString()));
     }
 }
