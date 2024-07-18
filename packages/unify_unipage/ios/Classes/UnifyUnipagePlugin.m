bbfc220d7e7b863e4e4bad9d3e2b25a52eff4624
“zhugeafanti” <liuruigang@hotmail.com>
2024-07-18 10:12:57 +0800

    modify: 【iOS】Delete unused code

--- packages/unify_unipage/ios/Classes/UnifyUnipagePlugin.m
+++ packages/unify_unipage/ios/Classes/UnifyUnipagePlugin.m
@@ -27,20 +27,6 @@ static NSMutableDictionary<NSString*, Class> *pageRegister;
             return [[cls alloc] initWithWithFrame:frame viewIdentifier:viewId viewType:key arguments:args binaryMessenger:[registrar messenger]];
         }] withId:key];
     }
-    
-    FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"unify_unipage"
-            binaryMessenger:[registrar messenger]];
-    UnifyUnipagePlugin* instance = [[UnifyUnipagePlugin alloc] init];
-    [registrar addMethodCallDelegate:instance channel:channel];
-}
-
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-    if ([@"getPlatformVersion" isEqualToString:call.method]) {
-        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-    } else {
-        result(FlutterMethodNotImplemented);
-    }
 }
 
 @end
