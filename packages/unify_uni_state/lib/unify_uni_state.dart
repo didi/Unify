9757aae9fe0c78991b5c7b3d06f48b8bf6316cd6
WangRui <wangruimaxiee@didiglobal.com>
2025-05-02 12:01:49 +0800

    UniState eventBus Flutter 侧实现

--- packages/unify_uni_state/lib/unify_uni_state.dart
+++ packages/unify_uni_state/lib/unify_uni_state.dart
@@ -1,9 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/services.dart';
 import 'package:unify_uni_bus/unify_uni_bus.dart';
 
 class UniState {
   static const String _uniStateEvent = 'unify_uni_state_event';
   static const String _uniStateChannel = 'unify_uni_state_channel';
+
+  static const String _eventKeyType = 'type';
+  static const String _eventKeyState = 'state';
+  static const String _eventKeyValue = 'value';
+  static const String _eventTypeOnStateChange = 'onStateChange';
 
   // 私有构造函数
   UniState._internal()
@@ -23,6 +30,20 @@ class UniState {
 
   final MethodChannel _methodChannel;
 
+  // 所有状态的事件流
+  // Key: stateKey, value: Stream<dynamic>
+  final Map<String, StreamController> _StateStreams = {};
+
+  Future<void> set(String key, dynamic value) async {
+  }
+
+  Future<dynamic> read(String key) async {
+
+  }
+
+  Future<Stream?> watch(String key) async {
+  }
+
   // 初始化平台事件通道
   void _initPlatformEventChannel() {
     _methodChannel.setMethodCallHandler((call) async {
@@ -32,6 +53,27 @@ class UniState {
 
   void _initEventBus() {
     // 监听 EventBus 事件
-    UniBus.instance.on(_uniStateEvent).listen((stateAction) {});
+    UniBus.instance.on(_uniStateEvent).listen((stateAction) {
+      String? type = stateAction[_eventKeyType];
+      if (type != _eventTypeOnStateChange) {
+        return;
+      }
+
+      String? state = stateAction[_eventKeyState];
+      if (state == null) {
+        return;
+      }
+
+      // 创建状态流
+      _createStreamIfNotExisted(state);
+      // 将状态值发送到状态流
+      _StateStreams[state]!.add(stateAction[_eventKeyValue]);
+    });
+  }
+
+  void _createStreamIfNotExisted(String stateKey) {
+    if (!_StateStreams.containsKey(stateKey)) {
+      _StateStreams[stateKey] = StreamController<dynamic>.broadcast();
+    }
   }
 }
