7fc80d33d9bc1d81e055610d76f77015c037fb26
WangRui <wangruimaxiee@didiglobal.com>
2025-05-02 14:30:08 +0800

    UniState 完善拦截器，set 改为可选

--- packages/unify_uni_state/android/src/main/java/com/didi/unify_uni_state/UniStateInterceptor.java
+++ packages/unify_uni_state/android/src/main/java/com/didi/unify_uni_state/UniStateInterceptor.java
@@ -1,7 +1,7 @@
 package com.didi.unify_uni_state;
 
-public interface UniStateInterceptor {
-    void set(String stateKey, Object stateValue);
+public abstract class UniStateInterceptor {
+    public void set(String stateKey, Object stateValue) {}
 
-    Object get(String stateKey);
+    public abstract Object get(String stateKey);
 }
