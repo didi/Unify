f7e692511f406a488fdc6d919c629b344285dec0
WangRui <wangruimaxiee@didiglobal.com>
2025-06-16 19:17:23 +0800

    bugfix: Dart UniBus.on allow null data

--- packages/unify_uni_bus/lib/unify_uni_bus.dart
+++ packages/unify_uni_bus/lib/unify_uni_bus.dart
@@ -54,11 +54,11 @@ class UniBus {
   /// 监听指定事件
   ///
   /// [eventName] 事件名称
-  Stream<Map<String, dynamic>?> on(String eventName) {
+  Stream<Map<String, dynamic>> on(String eventName) {
     // 返回包含该事件数据的流
     return _streamController.stream
         .where((event) => event['eventName'] == eventName)
-        .map((event) => event['data']);
+        .map((event) => event['data'] ?? {});
   }
 
   /// 发送事件
